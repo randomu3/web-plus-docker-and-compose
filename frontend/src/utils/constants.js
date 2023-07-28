a2017c953baec6e923f23594d6b8b323833c1889
randomu3 <demiz99@mail.ru>
2023-07-28 13:48:35 +0500

    change url for api frontend

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const URL = "https://api.grottie.nomoredomains.work";
+export const URL = "https://api.fr.grottie.nomoredomains.xyz";
 
 export const MINIMUM_PASSWORD_LENGTH = 3;
 export const MINIMUM_USERNAME_LENGTH = 3;
